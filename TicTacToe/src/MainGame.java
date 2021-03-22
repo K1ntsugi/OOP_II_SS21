2a78eea846690df0efb84362cba76f2baee9cfc3
K1ntsugi <max-groetsch@gmx.de>
2021-03-22 14:02:40 +0100

    Project done tictactoe (Java Swing)

---
+++ TicTacToe/src/MainGame.java
@@ -0,0 +1,10 @@
+
+public class MainGame {
+
+	public static void main(String[] args) {
+		
+		TicTacToe tic01 = new TicTacToe();
+		
+	}
+
+}
---
+++ vrl_noGUI/vrl_02/operatoren/Typumwandlungen.java
@@ -0,0 +1,15 @@
+package operatoren;
+
+public class Typumwandlungen {
+
+	public static void main(String[] args) {
+
+		int i = (int) 7.823;
+		float c = (float) 3 / 2 + 2.5F;
+
+		System.out.println(i);
+		System.out.println(c);
+
+	}
+
+}
