28e020c35220af0502ff77716c5c3f9ca3379d83
K1ntsugi <max-groetsch@gmx.de>
2021-05-11 16:32:27 +0200

    Stand: 11.05.21: image Flow übung alle features implementiert

--- vrl_03.05.21_ZusatzaufgabenEventhandling/src/application/Main.java
+++ vrl_03.05.21_ZusatzaufgabenEventhandling/src/application/Main.java
@@ -4,13 +4,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
@@ -18,16 +19,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	FlowPane iAmGroth;
-	TextField txt;
+	Scene scene;
+	static FlowPane iAmGroth;
+	static TextField txt;
 	Button add;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			txt = new TextField();
+			txt.setOnKeyPressed(e -> addPerEnter(e));
 			add = new Button();
-			add.setOnAction(e -> btnAdd(e));
+			add.setOnAction(e -> btnAdd());
 			add.setPrefSize(80, 10);
 			add.setText("add pic");
 			iAmGroth = new FlowPane();
@@ -38,12 +41,14 @@ public class Main extends Application {
 				public void handle(MouseEvent event) {
 					if (event.getButton() == MouseButton.PRIMARY) {
 						iAmGroth.getChildren().remove(event.getTarget());
+
 					}
 
 				}
 
 			});
-			Scene scene = new Scene(iAmGroth, 800, 800);
+
+			scene = new Scene(iAmGroth, 800, 800);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Adding images to a flowpane");
 			primaryStage.show();
@@ -52,20 +57,49 @@ public class Main extends Application {
 		}
 	}
 
+	public static void addPerEnter(KeyEvent e) {
+		if (e.getCode().equals(KeyCode.ENTER)) {
+			btnAdd();
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-	private void btnAdd(ActionEvent e) {
+	private static void btnAdd() {
 		try {
 			Image pic;
 			pic = new Image(new FileInputStream(txt.getText()));
 			ImageView container = new ImageView(pic);
+			container.setPreserveRatio(true);
+			container.setFitHeight(200);
+			container.setFitWidth(200);
 			container.setImage(pic);
 			iAmGroth.getChildren().add(container);
+
+			container.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					container.setOpacity(0.5);
+				}
+			});
+
+			container.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					container.setOpacity(100);
+
+				}
+			});
+
+			txt.clear();
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
 
 	}
+
 }
